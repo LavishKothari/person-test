9f6400f5fff161a5861c9be5c4eddb8fbe294fc6
lavishkothari004@gmail.com <lavishkothari004@gmail.com>
2019-06-22 13:30:22 +0530

    also added equals() and hashCode()

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Person {
     private final String name;
     private final int age;
@@ -13,5 +15,19 @@ public class Person {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 }
